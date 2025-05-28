9400f648f3928ddb88d6930ef1cb9c13f65b4834
abdounah <abdounah102030@gmail.com>
2025-05-28 16:24:25 +0200

    Added admin page routes

---
+++ src/admin/pages/single/Single.jsx
@@ -0,0 +1,13 @@
+import "./Single.scss";
+
+import React from "react";
+
+const Single = () => {
+  return (
+    <>
+      <div className="single">Single</div>
+    </>
+  );
+};
+
+export default Single;
