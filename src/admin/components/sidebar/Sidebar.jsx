0541798ca4dac0feaa48a92f69233205d0c7c5f4
abdounah <abdounah102030@gmail.com>
2025-05-28 21:49:30 +0200

    components part 1

---
+++ src/admin/components/sidebar/Sidebar.jsx
@@ -0,0 +1,16 @@
+import "./Sidebar.scss";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="sidebar">
+        <div className="top">
+            <span className="logo">UN-FST</span>
+        </div>
+        <div className="bottom">bottom</div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
