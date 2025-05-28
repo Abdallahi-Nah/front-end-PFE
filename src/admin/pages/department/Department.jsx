af256f48cdff370467ee5be5ae8d2e1cca7ea3b6
abdounah <abdounah102030@gmail.com>
2025-05-28 15:35:11 +0200

    Creating admin pages

---
+++ src/admin/pages/department/Department.jsx
@@ -0,0 +1,11 @@
+import "./Department.scss";
+
+const Department = () => {
+  return (
+    <>
+      <div className="department">Department</div>
+    </>
+  );
+};
+
+export default Department;
---
+++ src/admin/pages/module/Module.jsx
@@ -0,0 +1,11 @@
+import "./Module.scss";
+
+const Module = () => {
+  return (
+    <>
+      <div className="module">Module</div>
+    </>
+  );
+};
+
+export default Module;
